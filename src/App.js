627551a1239f88c05fd68079b9c751ecacc09644
Abhinand Shaji <abhinandshaji369@gmail.com>
2025-03-16 14:44:04 +0530

    data editing in firebase database(Firestore database)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { db } from './Firebase/config';
-import { collection, getDocs, addDoc } from "firebase/firestore";
+import { collection, getDocs, addDoc, doc, deleteDoc, updateDoc } from "firebase/firestore";
 
 function App() {
   const fetchData = () => {
@@ -24,13 +24,35 @@ function App() {
       console.error("Error adding document: ", error);
     });
   };
-  
+  // Update Data
+  const updateData = (docId) => {
+    updateDoc(doc(db, "products", docId), { price: 45000 })
+    .then(() => {
+      console.log(`Document with ID ${docId} updated successfully.`);
+    })
+    .catch((error) => {
+      console.error("Error updating document: ", error);
+    });
+  };
+
+  // Delete Data
+  const deleteData = (docId) => {
+    deleteDoc(doc(db, "products", docId))
+    .then(() => {
+      console.log(`Document with ID ${docId} deleted successfully.`);
+    })
+    .catch((error) => {
+      console.error("Error deleting document: ", error);
+    });
+  };
 
   return (
     <div>
       <h1>Abhinand</h1>
-      <button onClick={fetchData}>Click me</button>
-      <button onClick={writeData}>Click me</button>
+      <button onClick={fetchData}>Get</button>
+      <button onClick={writeData}>Add</button>
+      <button onClick={() => updateData("49fcp3qdNpohsTeuNZpu")}>Update Product</button>
+      <button onClick={() => deleteData("your_document_id_here")}>Delete Product</button>
     </div>
   );
 }
